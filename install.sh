06aaa7157a702c0a10769ec63e6cec90fd237c1b
GitOps <gitops@lamp.local>
2026-02-25 12:06:37 +0000

    Add automated installer

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-# Tento skript spustí inštalačný kontajner so všetkými právami
+# Jeden riadok, ktorý spustí celú mašinériu
 docker run --rm --privileged \
-  -v /:/host \
-  -v /run/systemd:/run/systemd \
+  -v /:/host -v /run/systemd:/run/systemd \
   -v /etc/rancher/k3s:/etc/rancher/k3s \
   -v /usr/local/bin:/usr/local/bin \
   --net=host \
